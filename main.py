d01c4120942e275b6dd45aed68e25b307df7c1bb
moonlin <moonlin.coder@gmail.com>
2023-03-31 17:25:07 +0300

    вынес общие переменные

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
-from windows import MenuWindow, GameWindow
-import time
 import pygame
+from windows import *
 from players import base
-import random
+
+WIDTH, HEIGHT = 1171, 700
+GAME_TITLE = "Футбол головой на двоих"
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+# all_sprites = pygame.sprite.Group()
+# player = base.Player()
+# all_sprites.add(player)
+
+running = True
+display = Menu.MenuWindow(screen)
+
+while running:
+    for event in pygame.event.get():
+        # проверить закрытие окна
+        if event.type == pygame.QUIT:
+            running = False
+
+    display.update()
+    pygame.time.wait(1000)
+    clock.tick(60)
+    pygame.display.update()
+    screen.fill((20, 0, 0))
+
+    # button_3 = pygame.Rect(400, 420, 200, 50)
+    # pygame.draw.rect(screen, (153, 186, 221), button_3)
+    #
+    # all_sprites.update()
+    # all_sprites.draw(screen)
 
 
-def start():
-    mainClock = pygame.time.Clock()
-    pygame.init()
-    screen = pygame.display.set_mode((1000, 600))
-    pygame.display.set_caption("Футбол головой на двоих")
 
-    all_sprites = pygame.sprite.Group()
-    player = base.Player()
-    all_sprites.add(player)
-
-    running = True
-    
-    while running:
-        for event in pygame.event.get():
-            # проверить закрытие окна
-            if event.type == pygame.QUIT:
-                running = False
-
-        screen.fill((20, 0, 0))
-
-        button_3 = pygame.Rect(400, 420, 200, 50)
-        pygame.draw.rect(screen, (153, 186, 221), button_3)
-
-        all_sprites.update()
-        all_sprites.draw(screen)
-
-        pygame.display.update()
-        mainClock.tick(60)
-
-
-if __name__ == '__main__':
-    start()
